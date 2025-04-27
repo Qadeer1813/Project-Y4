45975315d82f5e5e1d4fa2c50ff2acf340057392
Qadeer1813 <c00270632@itcarlow.ie>
2025-04-27 03:05:55 +0100

    acciedntal file upload fix

--- Elderly Care Management System/CareNet/CareNet/config.py
+++ Elderly Care Management System/CareNet/CareNet/config.py
@@ -1,12 +1,12 @@
 DB_CONFIG = {
-    'user': 'root',
-    'password': 'Qadeerh03',
-    'host': 'localhost',
-    'database': 'carenet',
+    'user': '',
+    'password': '',
+    'host': '',
+    'database': '',
     'port': 3306
 }
-API_TOKEN = "TPM2HgNtx7snEu5m3rC_9py4YCwiB_9rt_tm616whPM"
-API_BASE_URL = "http://localhost:8001"
+API_TOKEN = ""
+API_BASE_URL = ""
 API_ENDPOINTS = {
     'current_key': f"{API_BASE_URL}/current-key",
     'check_key': f"{API_BASE_URL}/check-key",
